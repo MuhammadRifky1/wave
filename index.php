b2df00ec62991b43cd525f86291a1a2488f99cc3
MuhammadRifky1 <rifky.rangkuti@gmail.com>
2019-01-22 22:47:04 +0700

    wave

---
+++ index.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Wave Learning Site | Zubair Tech</title>
+    <link rel="stylesheet" href="style.css" type="text/css">
+    
+</head>
+<body>
+    <!-- Preloader -->
+    <div class="preloader-container" id="overlay">
+            <div class="box-preloader" id="ball"></div>
+            <div class="box-preloader" id="ball2"></div>
+    </div>
+    <!-- /Preloader -->
+    <section class="title-section">
+        <div class="title-container">
+            <div class="flex-center">
+              <strong>gelombang elektromagnetik</strong>
+            </div>
+        </div>
+    </section>
+
+    <div class="container">
+        <div class="content">
+            <div class="left-content">
+                    <img src="scientist.jpg" width="400px"/>
+            </div>
+            <div class="right-content">
+                <div class="box-desc">
+                    <p>
+                        <div class="equation pull-left"></div>
+
+                        <div id="listingTable"></div>
+                        <a href="javascript:prevPage()" id="btn_prev">Prev</a>
+                        <a href="javascript:nextPage()" id="btn_next">Next</a>
+                        page: <span id="page"></span>
+                    
+                    
+                    
+                    </p>
+                </div>
+            </div>
+            <div class="pull-right">
+
+                
+            </div>
+            
+        </div>
+    </div>
+    
+
+
+
+
+
+
+
+
+<script src="gsap/TweenMax.min.js" type="text/javascript"></script>
+<script src="control.js" type="text/javascript"></script>
+</body>
+</html> 
