03e5532aee78b4cf0932f855ed26ef6897faf55a
Rifky <rifky.rangkuti@gmail.com>
2019-01-24 21:55:50 +0700

    4

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
 </head>
 <body>
     <!-- Preloader -->
-    <!-- <div class="preloader-container" id="overlay">
+    <div class="preloader-container" id="overlay">
             <div class="box-preloader" id="ball"></div>
             <div class="box-preloader" id="ball2"></div>
-    </div> -->
+    </div>
     <!-- /Preloader -->
     <section class="title-section">
         <div class="title-container">
@@ -25,7 +25,7 @@
     <div class="container">
         <div class="content">
             <div class="left-content">
-                    <img src="scientist.jpg" width="300vw"/>
+                    <img src="scientist.jpg" width="300vh"/>
             </div>
             <div class="right-content">
                 <div class="box-desc">
@@ -49,6 +49,41 @@
                 <div class="hal pull-right"><span id="page_num">asd</span></div>
             </div>
             
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="content">
+            <div class="left-content">
+                <div class="box-q-list">
+                    <ul>
+                        <li>Pertannyaan ke-1</li>
+                        <li>Pertannyaan ke-2</li>
+                        <li>Pertannyaan ke-3</li>
+                        <li>Pertannyaan ke-4</li>
+                        <li>Pertannyaan ke-5</li>
+                        <li>Pertannyaan ke-6</li>
+                        <li>Pertannyaan ke-7</li>
+                        <li>Pertannyaan ke-8</li>
+                        
+                    </ul>
+                </div>
+            </div>
+            <div class="right-content">
+                <div class="box-q">
+                    <p id="desc-q">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat numquam sit non consequatur explicabo minima quod voluptate similique dicta, impedit porro suscipit facilis. Cum quaerat saepe ea delectus, officia nam.</p>
+
+                    <form action="">
+                        <input type="radio" name="gender" value="male"> Male<br>
+                        <input type="radio" name="gender" value="female"> Female<br>
+                        <input type="radio" name="gender" value="other"> Other
+                    </form>
+                </div>
+            </div>
+            <div class="pull-right">
+                <input type="button" class="button-paginate" id="next" onclick="nextPage()" value="next" />
+                <input type="button" class="button-paginate" id="previous" onclick="previousPage()" value="previous" />
+            </div>
         </div>
     </div>
     
