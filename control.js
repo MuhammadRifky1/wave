740ab3adbc61807a376d6e5aeaff7a73f277350e
Rifky <rifky.rangkuti@gmail.com>
2019-01-29 15:16:56 +0700

    paralax

--- control.js
+++ control.js
@@ -1,32 +1,56 @@
-var ball = document.getElementById('ball'),
-    ball2 = document.getElementById('ball2'),
-    preloader = document.getElementById('overlay');
+// var ball = document.getElementById('circle1'),
+//     ball2 = document.getElementById('circle2'),
+//     ball3 = document.getElementById('circle3'),
+//     ball4 = document.getElementById('circle4'),
+//     ball5 = document.getElementById('circle5'),
+//     preloader = document.getElementById('sb'),
+//     ts = document.getElementById('title-section'),
+//     hd = document.getElementById('header'),
+//     title = document.getElementById('title-text'),
+//     ti = document.getElementById('tower-img'),
+//     tlm = new TimelineMax();
+    
 
 
 
 
-var tm = TweenMax.from(ball2, 1.8,{ ease: Bounce.easeOut, y: -400, repeat: -1});
-         TweenMax.from(ball, 1.5,{ ease: Bounce.easeOut, y: -400, repeat: -1, repeatDelay: .3});
+    
+    
+//     setTimeout(function wait_light(){
+//         TweenMax.to(ball, 0.5,{scale:10, ease:ExpoScaleEase.config(1, 2)});
+//         TweenMax.to(ball2, 0.5,{scale:10, ease:ExpoScaleEase.config(1, 2)});
+//         TweenMax.to(ball3, 0.5,{scale:20, ease:ExpoScaleEase.config(1, 2)});
+//         TweenMax.to(ball4, 0.5,{scale:10, ease:ExpoScaleEase.config(1, 2)});
+//         TweenMax.to(ball5, 0.5,{scale:10, ease:ExpoScaleEase.config(1, 2)});
+//         ball.style.display = "inline";
+//         ball2.style.display = "inline";
+//         ball3.style.display = "inline";
+//         ball4.style.display = "inline";
+//         ball5.style.display = "inline";
+//     }, 4000);
+
+    
+//    function blow(){
+//         setTimeout(function(){
+//             preloader.style.display = "none";
+//         }, 4600);
+//         setTimeout(function(){
+//             TweenMax.from(hd, .5, { ease: Power1.easeOut, y: -20});
+//             TweenMax.from(title, .5, { ease: Power1.easeOut, y: -20});
+//             TweenMax.from(ti, .5, { ease: Power1.easeOut, y: 20});
+//         }, 4601);
+//    }
+   
+        
+   
+   
 
 
-function blow(){
-    setTimeout(function(){
-        tm.kill();
-        ball.style.display = "none";
-        TweenMax.to(ball2, 1,{scale: 45, alpha: 1});
-        DestroyPreloader();
-    }, 1800)
-}
 
 
 
-function DestroyPreloader(){
-    setTimeout(function(){
-        preloader.style.display = 'none';
-    }, 1000)
-}
+// window.onload = blow();
+       
 
-window.onload = blow();
-
-//ScrollMagic 
+// //ScrollMagic 
 
--- sm.js
+++ sm.js
@@ -1,37 +1,44 @@
 var controller = new ScrollMagic.Controller();
 
-// create a scene
-var wipeAnimation = new TimelineMax()
-    .from("#sb", 1,   {x: "100%"})
-var pinTitle = new ScrollMagic.Scene({
-        triggerHook: 'onLeave',
-        triggerElement: '#title-section',
-        duration: '200%'
-    })
-    .setPin("#title-section")
-    .addIndicators()
-    .addTo(controller);
+// // create a scene
 
-var pinSb = new ScrollMagic.Scene({
-        triggerHook: "onLeave",
-        duration: "100%"
-    })
-    .setPin("#sb", {pushFollowers: false})
-    .setTween(wipeAnimation)
-    .addIndicators()
-    .addTo(controller);
 
-// var load = new ScrollMagic.Scene({
-//     triggerHook: 0,
-//     triggerElement: "#transition_coming"
-// })
-// .setClassToggle("#sb", "show-flex")
-// .addIndicators({
-//     name: "load",
-//     colorTrigger: "red",
-//     colorTrigger: "red"
-// })
-// .addTo(controller);
+new ScrollMagic.Scene({
+    duration: "100%",
+    triggerHook: 1   
+  })
+  .setTween('#left-content', {y: "-100%", ease: Linear.easeNone})
+  .addIndicators()
+  .addTo(controller);
+new ScrollMagic.Scene({
+    duration: "100%",
+    triggerHook: 1   
+  })
+  .setTween('#mid-content', {y: "-30%", ease: Linear.easeNone})
+
+  .addTo(controller);
+
+new ScrollMagic.Scene({
+    duration: "100%",
+    triggerHook: 1   
+  })
+  .setTween('#right-content', {y: "-70%", ease: Linear.easeNone})
+  .addIndicators()
+  .addTo(controller);
+
+
+
+// // var load = new ScrollMagic.Scene({
+// //     triggerHook: 0,
+// //     triggerElement: "#transition_coming"
+// // })
+// // .setClassToggle("#sb", "show-flex")
+// // .addIndicators({
+// //     name: "load",
+// //     colorTrigger: "red",
+// //     colorTrigger: "red"
+// // })
+// // .addTo(controller);
 
 
 
