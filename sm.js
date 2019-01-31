fb66bdc3e9490ca64efd1bc53aad421c2eecf2f3
MuhammadRifky1 <rifky.rangkuti@gmail.com>
2019-01-31 08:29:45 +0700

    hampir

--- sm.js
+++ sm.js
@@ -4,7 +4,7 @@ var tm = new TimelineMax()
 
 
 var circle = tm.staggerTo(".circle",3, {scale: 20, onComplete: destroy_circle}, 0.3);
-var circle_alpha = TweenMax.staggerFrom(".circle",1.5, { alpha:0 }, 0.3);
+var circle_alpha = TweenMax.staggerFrom(".circle",10, { alpha:0 }, 0.3);
 function destroy_circle(){
   for(i = 0; i < 7; i++){
       document.getElementsByClassName("circle")[i].style.display = "none";
@@ -27,9 +27,30 @@ materi.addEventListener('click', function(){
   var element = document.getElementById('Materi');
 element.scrollIntoView({behavior: "smooth", block: "start"});
 })
-// create a scene
+
+// Gelombang Elektromagnetik
+new ScrollMagic.Scene({
+  triggerElement: '#header',
+  offset: 100,
+  triggerHook: 0,
+  duration: "50%",
+})
+.setTween(circle)
+.addIndicators()
+.addTo(controller);
+
+new ScrollMagic.Scene({
+  triggerElement: '#header',
+  offset: 100,
+  triggerHook: 0,
+  duration: "50%",
+})
+.setTween(circle_alpha)
+.addIndicators()
+.addTo(controller);
 
 
+// paralax
 new ScrollMagic.Scene({
     triggerElement: '#paralax',
     duration: "100%",
@@ -37,16 +58,6 @@ new ScrollMagic.Scene({
   })
   .setTween('#left-content', {y: "-20%", ease: Linear.easeNone})
   .addTo(controller);
-//toDownWave5
-new ScrollMagic.Scene({
-  triggerElement: '#paralax',
-  duration: "110%",
-  triggerHook: 1
-})
-.setTween('#toDown', {y: "100%", ease: Linear.easeNone})
-
-.addTo(controller);
-
 new ScrollMagic.Scene({
     triggerElement: '#paralax',
     duration: "100%",
@@ -63,32 +74,39 @@ new ScrollMagic.Scene({
   .setTween('#right-content', {y: "-50%", ease: Linear.easeNone})
   .addTo(controller);
 
-
-
-
-new ScrollMagic.Scene({
-  triggerElement: '#header',
-  offset: 100,
-  triggerHook: 0,
-  duration: "50%",
-})
-.setTween(circle)
-.addIndicators()
-.addTo(controller);
-
-
-
-
-
-
-
-// fade-in
+// text-reveal
 new ScrollMagic.Scene({
   triggerElement: '#Materi',
   triggerHook: .1   
 })
 .setTween('#text-reveal', { ease: Power1.easeOut, y: 0})
 .addTo(controller);
+
+//wave
+new ScrollMagic.Scene({
+  triggerElement: '#paralax',
+  duration: "110%",
+  triggerHook: 1
+})
+.setTween('#toDown', {y: "100%", ease: Linear.easeNone})
+.addTo(controller);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // // var load = new ScrollMagic.Scene({
 // //     triggerHook: 0,
